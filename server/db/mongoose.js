62c901c67407092e7cfd7d885df2c3f7b2344b39
Alvaro Rene <alvaro.kom@gmail.com>
2017-04-27 18:02:28 -0500

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://alvaro:dsm@ds123331.mlab.com:23331/todoapp');
+
 
 mongoose.Promise = global.Promise
 
