488783dd8a527b5c417c5bc8a18d3ea01dc65a83
Alvaro Rene <alvaro.kom@gmail.com>
2017-04-27 19:42:59 -0500

    tweak mongoose URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose')
 
- // mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://alvaro:dsm@ds123331.mlab.com:23331/todoapp');
+var URI = "mongodb://localhost:27017/TodoApp"
+
+if(process.env.PORT){
+    URI = "mongodb://alvaro:dsm@ds123331.mlab.com:23331/todoapp"
+}
+
+
+mongoose.connect(URI);
 
 
 mongoose.Promise = global.Promise
