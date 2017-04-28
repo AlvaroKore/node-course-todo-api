a13dbbe13b119e0d6e426678114ef800ac72179a
Alvaro Rene <alvaro.kom@gmail.com>
2017-04-27 19:33:31 -0500

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
 
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://alvaro:dsm@ds123331.mlab.com:23331/todoapp');
+ mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://alvaro:dsm@ds123331.mlab.com:23331/todoapp');
 
 
 mongoose.Promise = global.Promise
