967b38a38647773247b20e257ca2b0e249dab7f5
Alvaro Rene <alvaro.kom@gmail.com>
2017-04-28 20:49:18 -0500

    Setup separate test and developments envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,8 @@
 var mongoose = require('mongoose')
 
-var URI = "mongodb://localhost:27017/TodoApp"
-
-if(process.env.PORT){
-    URI = "mongodb://alvaro:dsm@ds123331.mlab.com:23331/todoapp"
-}
 
 
-mongoose.connect(URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 
 mongoose.Promise = global.Promise
