06bffdfaae85cee93f061ccb1ad82db5d1c8c4b2
Alvaro Rene <alvaro.kom@gmail.com>
2017-05-18 19:31:01 -0500

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,24 @@
 const env = process.env.NODE_ENV || 'development'
 console.log('env*****', env)
 
-if(env === 'development'){
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp"
-} else if (env === 'test') {
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest"
+if (env  === 'development' || env === 'test' || env === 'production' ){
+    var config = require('./config.json')
+    var envConfig = config[env]
 
-} else if (env === 'production') {
-    process.env.MONGODB_URI = "mongodb://alvaro:dsm@ds123331.mlab.com:23331/todoapp"
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key]
+    })
 
 }
+
+// if(env === 'development'){
+//     process.env.PORT = 3000
+//     process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp"
+// } else if (env === 'test') {
+//     process.env.PORT = 3000
+//     process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest"
+//
+// } else if (env === 'production') {
+//     process.env.MONGODB_URI = "mongodb://alvaro:dsm@ds123331.mlab.com:23331/todoapp"
+//
+// }
